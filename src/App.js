41709bd2274a26eb11eb755aee2ba760fe45802f
giuliabriao <briaogiulia@gmail.com>
2021-05-02 19:25:54 -0300

    Header e CSS Grid feitos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import './App.css';
 import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 class App extends React.Component {
-  somaCompras(comprasCliente){
-    const comprasTotal = comprasCliente.reduce((acc, {preco}) =>  acc + Number(preco), 0);
+  somaCompras(comprasCliente) {
+    const comprasTotal = comprasCliente.reduce((acc, { preco }) => acc + Number(preco), 0);
     return comprasTotal
   }
 
-  renderColorValue(somaCompras){
-    if(somaCompras > 10000){
-      return <span style={{color: "red"}}>Você consumiu acima de R$10.000</span>
-    }else{
-      return <span style={{color: "green"}}>Você consumiu abaixo de R$10.000</span>
+  renderColorValue(somaCompras) {
+    if (somaCompras > 10000) {
+      return <span style={{ color: "red", fontSize: "10px" }}>Você consumiu acima de R$10.000</span>
+    } else {
+      return <span style={{ color: "green", fontSize: "10px" }}>Você consumiu abaixo de R$10.000</span>
     }
   }
 
   render() {
-    const styleTitle = {
-      color: 'blue',
-      fontSize: '20px',
-      fontFamily: 'Fantasy'
-    };
-
     const clienteJoao = {
       nome: 'João',
       idade: 37,
@@ -46,35 +41,36 @@ class App extends React.Component {
       cidade: "Rio de Janeiro",
     };
 
-    const {nome: joao, idade: idadeJoao, compras: comprasJoao} = clienteJoao;
-    const {nome: maria, idade: idadeMaria, compras: comprasMaria} = clienteMaria;
+    const { nome: joao, idade: idadeJoao, compras: comprasJoao } = clienteJoao;
+    const { nome: maria, idade: idadeMaria, compras: comprasMaria } = clienteMaria;
 
     const somaComprasJoao = this.somaCompras(comprasJoao);
     const somaComprasMaria = this.somaCompras(comprasMaria);
 
-      return (
-        <div>
-          <h1 style={styleTitle}>Clientes Magazine Giuliza</h1>
-          <div>
+    return (
+      <div className="grid-container">
+        <Header />
+          <div className="customer1">
             <p>A cliente {maria}:</p>
             <ul>
               <li>Tem {idadeMaria} anos;</li>
               <li>E consumiu o total de R${somaComprasMaria}</li>
               {this.renderColorValue(somaComprasMaria)}
-              
             </ul>
-    
+          </div>
+
+          <div className="customer2">
             <p>O cliente {joao}:</p>
             <ul>
               <li>Tem {idadeJoao} anos;</li>
               <li>E consumiu o total de R${somaComprasJoao}.</li>
               {this.renderColorValue(somaComprasJoao)}
             </ul>
-            < Footer />
-          </div>
         </div>
-        );
-    }
-  }    
+        < Footer />
+      </div>
+    )
+  }
+}
 
 export default App;
